f194ecf660e232f1b1caee32f2f8537b6a5e1d75
dtao <daniel.tao@gmail.com>
2011-02-02 15:58:37 -0800

    adding example of how calling Control.Invoke can result in deadlock if not handled properly

---
+++ InvokeDeadlockExample/DeadlockExampleForm.Designer.vb
@@ -0,0 +1,55 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class DeadlockExampleForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.BigButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'BigButton
+        '
+        Me.BigButton.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+                    Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.BigButton.Location = New System.Drawing.Point(21, 21)
+        Me.BigButton.Margin = New System.Windows.Forms.Padding(12)
+        Me.BigButton.Name = "BigButton"
+        Me.BigButton.Size = New System.Drawing.Size(242, 220)
+        Me.BigButton.TabIndex = 0
+        Me.BigButton.Text = "Illustrate Deadlock"
+        Me.BigButton.UseVisualStyleBackColor = True
+        '
+        'DeadlockExampleForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.BigButton)
+        Me.Name = "DeadlockExampleForm"
+        Me.ShowIcon = False
+        Me.Text = "Invoke/Deadlock Example"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Private WithEvents BigButton As System.Windows.Forms.Button
+
+End Class
---
+++ InvokeDeadlockExample/DeadlockExampleForm.vb
@@ -0,0 +1,39 @@
+﻿Imports System.Threading
+
+Public Class DeadlockExampleForm
+
+    Private _lock As New Object
+    Private _unlocked As Boolean
+
+    Private Sub BigButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BigButton.Click
+        SyncLock _lock
+            Dim t As New Thread(AddressOf Deadlock)
+            t.Start()
+
+            MsgBox("Once you click 'OK' I will enter deadlock.", MsgBoxStyle.OkOnly, "Deadlock Incoming...")
+
+            ' This loop will wait forever.
+            While Not _unlocked
+                Thread.Sleep(1000)
+            End While
+        End SyncLock
+    End Sub
+
+    Private Sub Deadlock()
+        SyncLock _lock
+            ' Invoke is a blocking call. This thread will not continue past this
+            ' line until the lock on the UI thread is released. But since the
+            ' UI thread will not leave the button's Click event handler until
+            ' _unlocked is set to True below, this will never happen.
+            Invoke(New Action(AddressOf NotifyComplete))
+
+            ' This line will never be reached.
+            _unlocked = True
+        End SyncLock
+    End Sub
+
+    Private Sub NotifyComplete()
+        MsgBox("I escaped deadlock!", MsgBoxStyle.OkOnly, "No Deadlock?")
+    End Sub
+
+End Class
