36a9834bacaba25247c144acf3a86f6aa27d3402
dtao <dtao@thoughtworks.com>
2011-02-21 10:35:28 -0800

    simplified deadlock example (don't actually need SyncLock)

--- InvokeDeadlockExample/DeadlockExampleForm.vb
+++ InvokeDeadlockExample/DeadlockExampleForm.vb
@@ -2,36 +2,30 @@
 
 Public Class DeadlockExampleForm
 
-    Private _lock As New Object
     Private _unlocked As New ManualResetEvent(False)
 
     Private Sub BigButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BigButton.Click
-        SyncLock _lock
-            Dim t As New Thread(AddressOf Deadlock)
-            t.Start()
+        Dim t As New Thread(AddressOf Deadlock)
+        t.Start()
 
-            MsgBox("Once you click 'OK' I will enter deadlock.", MsgBoxStyle.OkOnly, "Deadlock Incoming...")
+        Me.Text = "Entering Deadlock"
 
-            ' This line will wait forever.
-            _unlocked.WaitOne()
-        End SyncLock
+        ' This line will wait forever.
+        _unlocked.WaitOne()
     End Sub
 
     Private Sub Deadlock()
-        SyncLock _lock
-            ' Invoke is a blocking call. This thread will not continue past this
-            ' line until the lock on the UI thread is released. But since the
-            ' UI thread will not leave the button's Click event handler until
-            ' _unlocked is signalled below, this will never happen.
-            Invoke(New Action(AddressOf NotifyComplete))
+        ' Invoke is a blocking call. This thread will not continue past this
+        ' line until the Click event handler above runs. Until _unlocked is
+        ' signalled below, this will never happen.
+        Invoke(New Action(AddressOf NotifyComplete))
 
-            ' This line will never be reached.
-            _unlocked.Set()
-        End SyncLock
+        ' This line will never be reached.
+        _unlocked.Set()
     End Sub
 
     Private Sub NotifyComplete()
-        MsgBox("I escaped deadlock!", MsgBoxStyle.OkOnly, "No Deadlock?")
+        Me.Text = "Escaped Deadlock"
     End Sub
 
 End Class
