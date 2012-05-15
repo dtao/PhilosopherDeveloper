af7ca1800bcfcc79b2558f052d2c17fe71988c67
Dan Tao <daniel.tao@gmail.com>
2012-05-15 08:45:21 -0700

    updated database.rb for PostgreSQL in production

--- config/database.rb
+++ config/database.rb
@@ -1,19 +1,8 @@
-##
-# A MySQL connection:
-# DataMapper.setup(:default, 'mysql://user:password@localhost/the_database_name')
-#
-# # A Postgres connection:
-# DataMapper.setup(:default, 'postgres://user:password@localhost/the_database_name')
-#
-# # A Sqlite3 connection
-# DataMapper.setup(:default, "sqlite3://" + Padrino.root('db', "development.db"))
-#
-
 DataMapper.logger = logger
 DataMapper::Property::String.length(255)
 
 case Padrino.env
+  when :production  then DataMapper.setup(:default, ENV["DATABASE_URL"])
   when :development then DataMapper.setup(:default, "sqlite3://" + Padrino.root('db', "blog_development.db"))
-  when :production  then DataMapper.setup(:default, "sqlite3://" + Padrino.root('db', "blog_production.db"))
   when :test        then DataMapper.setup(:default, "sqlite3://" + Padrino.root('db', "blog_test.db"))
 end
