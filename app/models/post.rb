6b19fc58d06c649420601c62276a3c0ba1470235
Dan Tao <daniel.tao@gmail.com>
2012-06-28 17:50:52 -0700

    revamped post to be file-based rather than DB-based (feels right)

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,47 @@
 class Post
-  include DataMapper::Resource
+  POSTS_ROOT = File.join(PADRINO_ROOT, "posts")
 
-  has n, :visits
+  @@all = {}
 
-  property :id,           Serial
-  property :created_at,   DateTime
-  property :permalink,    String, :unique => true, :index => true
-  property :title,        String
-  property :content,      Text
-  property :published,    Boolean
+  def self.load_all
+    Dir.glob(File.join(POSTS_ROOT, "*.markdown")).each do |file_path|
+      post = Post.new(File.basename(file_path))
+      @@all[post.id] = post
+    end
+  end
+
+  def self.get(id)
+    @@all[id]
+  end
+
+  attr_reader :date, :id, :title, :text
+  attr_accessor :rendered_html
+
+  private
+  def initialize(file)
+    date, title = file.match(/(\d{4}-\w{3}-\d{2}) (.*)\.markdown/)[1, 2]
+    year, month_abbr, day = date.split("-")
+
+    @date = Date.new(year.to_i, month_from_abbreviation(month_abbr), day.to_i)
+    @title = title
+    @id = @title.downcase.gsub(/\s+/, "-")
+    @text = File.read(File.join(POSTS_ROOT, file))
+  end
+
+  def month_from_abbreviation(abbr)
+    case abbr.downcase
+    when "jan" then 1
+    when "feb" then 2
+    when "mar" then 3
+    when "apr" then 4
+    when "may" then 5
+    when "jun" then 6
+    when "jul" then 7
+    when "aug" then 8
+    when "sep" then 9
+    when "oct" then 10
+    when "nov" then 11
+    when "dec" then 12
+    end
+  end
 end
