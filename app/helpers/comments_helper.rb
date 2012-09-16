9a950c9278051c19bb7aee849742733633798c9c
Dan Tao <daniel.tao@gmail.com>
2012-09-15 17:57:27 -0700

    fixed display of minutes for comments

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,11 +1,11 @@
-require "md5"
+require "digest/md5"
 
 module CommentsHelper
   def comment_header(comment)
     email = comment.reader_email || ""
-    hash  = MD5.new(email.strip.downcase).hexdigest
+    hash  = Digest::MD5.hexdigest(email.strip.downcase)
     url   = "http://www.gravatar.com/avatar/#{hash}?s=50&d=identicon"
-    time  = comment.created_at.strftime("%l:%m %p on %b %d, %Y")
+    time  = comment.created_at.strftime("%l:%M %p on %b %d, %Y")
     %Q{<img class="reader-pic" src="#{url}" /> <span class="reader-name">#{comment.reader_name}</span> <span class="comment-time">#{time}</span>}
   end
 end
