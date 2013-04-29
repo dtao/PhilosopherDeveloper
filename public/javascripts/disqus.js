d54b857e5e2723d250b3668bced48589912dce2f
Dan Tao <daniel.tao@gmail.com>
2013-04-29 08:50:19 -0700

    adding comment counts to posts
    hopefully this isn't a huge mistake

--- public/javascripts/disqus.js
+++ public/javascripts/disqus.js
@@ -1,6 +1,3 @@
-var disqus_shortname = 'philosopherdeveloper'; // required: replace example with your forum shortname
-
-/* * * DON'T EDIT BELOW THIS LINE * * */
 (function() {
   var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
   dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
