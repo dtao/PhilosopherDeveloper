92890d92aefc5c8f5ece9c1e733a445559cbc4dd
Dan Tao <daniel.tao@gmail.com>
2013-09-19 09:29:37 -0700

    went a little nuts...

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -1,6 +1,7 @@
 window.addEventListener('load', function() {
   var nav     = document.querySelector('nav'),
-      content = document.getElementById('content');
+      content = document.getElementById('content'),
+      article = content.querySelector('article');
 
   function scrolledPastNav() {
     return window.scrollY > (nav.offsetTop + nav.offsetHeight);
@@ -8,10 +9,43 @@ window.addEventListener('load', function() {
 
   function adjustContentWidth() {
     if (scrolledPastNav()) {
-      content.className = 'full-width';
-    } else {
-      content.removeAttribute('class');
+      makeContentFullWidth();
+
+    } else if (content.className === 'full-width') {
+      resetContentWidth();
     }
+  }
+
+  function makeContentFullWidth() {
+    // Exit early if we've already done this
+    if (content.className === 'full-width') {
+      return;
+    }
+
+    // Find the first visible child element
+    var topElement = findTopElement();
+    var distanceFromTop = window.scrollY - topElement.offsetTop;
+
+    // Expand the content element
+    content.className = 'full-width';
+
+    window.scrollTo(0, topElement.offsetTop + distanceFromTop);
+  }
+
+  function resetContentWidth() {
+    content.removeAttribute('class');
+  }
+
+  function findTopElement() {
+    var children = article.children;
+
+    for (var i = 0; i < children.length; ++i) {
+      if (children[i].offsetTop + children[i].offsetHeight > window.scrollY) {
+        return children[i];
+      }
+    }
+
+    return article.firstChild;
   }
 
   window.addEventListener('scroll', adjustContentWidth);
